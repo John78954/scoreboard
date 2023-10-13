c8fe18af859fdc9e644a4952b442a0c2117664b5
John Pena <jroger.pena@gmail.com>
2023-10-13 11:42:10 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+import ScoreboardView from "./scoreboard/ScoreboardView.js";
+
+let playerOneScore = 0;
+let playerTwoScore = 0;
+const root = document.querySelector("#app");
+const view = new ScoreboardView(root, "Player One", "Player Two", (player, direction) => {
+const difference = direction === "minus" ? -1 : 1;
+
+if (player === "one") {
+    playerOneScore = Math.max(playerOneScore + difference, 0);
+} else {
+    playerTwoScore = Math.max(playerTwoScore + difference, 0);
+}
+
+view.update(playerOneScore, playerTwoScore);
+    // Update the scores
+    // console.log(player)
+    // console.log(direction)
+});
+
+// view.update(12, 30);
